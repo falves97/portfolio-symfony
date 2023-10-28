d9e845d60dfb2eade9e2c044460c2aa68472cdc1
Fernando Braga Alves <fbragaalves97@gmail.com>
2023-10-28 18:25:26 -0300

    feat: add Admin Controller para Post

---
+++ src/Controller/Admin/PostCrudController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Post::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->onlyOnDetail(),
+            TextField::new('title'),
+            TextEditorField::new('content'),
+        ];
+    }
+
+}
